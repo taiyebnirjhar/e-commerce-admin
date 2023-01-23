95e2fd900feab8a47f21bc8c3565fbf15912007e
taiyebnirjhar <nirjhar.212902003.green.ac.bd>
2023-01-23 17:38:19 +0600

    regester page & firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Register />
     </div>
   );
 }
